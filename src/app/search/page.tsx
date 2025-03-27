6b920dd7531ca4bf1f884d47474f17d1bde69a5e
Arklimits <zaqokm2@gmail.com>
2025-03-27 17:14:55 +0900

    feat: 키워드 없을 때 메인화면으로 리디렉션션

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
 import SearchBar from "@/components/search/SearchBar";
 import CharacterCardContainer from "@/components/card/CharacterCardContainer";
 import "@/styles/globals.scss";
@@ -8,10 +9,17 @@ import styles from "./SearchPage.module.scss";
 
 const SearchPage = () => {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const keyword = searchParams.get("keyword")?.trim() ?? "";
 
+  useEffect(() => {
+    if (!keyword) {
+      router.push("/");
+    }
+  }, [keyword, router]);
+
   if (!keyword) {
-    return <div>검색어가 없습니다.</div>;
+    return <div>메인화면으로 돌아갑니다.</div>;
   }
 
   return (
