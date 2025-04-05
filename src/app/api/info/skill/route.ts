e2a53fb304ed4943cc08a463a2bdffefe20bb809
Arklimits <zaqokm2@gmail.com>
2025-04-05 18:08:19 +0900

    feat: DB에서 스킬 관련 정보 가져오는 API 구현 #10

---
+++ src/app/api/info/skill/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { db } from '@/lib/db';
+import { RowDataPacket } from 'mysql2';
+
+export async function POST(req: NextRequest) {
+  const { skills } = await req.json();
+
+  if (!Array.isArray(skills)) {
+    return NextResponse.json({ error: '잘못된 요청입니다.' }, { status: 400 });
+  }
+
+  const names = skills.map((s: any) => s.Name);
+  const [constants] = await db.query<RowDataPacket[]>(
+    `SELECT name, coefficient, constant, cooltime, casttime FROM skills WHERE name IN (${names.map(() => '?').join(',')})`,
+    names
+  );
+
+  const enriched = skills.map(skill => {
+    const constant = constants.find(c => c.name === skill.Name);
+
+    console.log(constant);
+    return {
+      ...skill,
+      ...constant,
+    };
+  });
+
+  return NextResponse.json(enriched);
+}
