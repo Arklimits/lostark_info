c0db23448867e533564123ce8c6e2bb267b9cdc2
Arklimits <zaqokm2@gmail.com>
2025-04-06 12:38:11 +0900

    chore: 디버그용 콘솔 제거거

--- src/app/api/info/skill/route.ts
+++ src/app/api/info/skill/route.ts
@@ -18,7 +18,6 @@ export async function POST(req: NextRequest) {
   const enriched = skills.map(skill => {
     const constant = constants.find(c => c.name === skill.Name);
 
-    console.log(constant);
     return {
       ...skill,
       ...constant,
