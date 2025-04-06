9cbcb1155dd25b38abe305e69cca4239060b58fd
Arklimits <zaqokm2@gmail.com>
2025-04-06 12:38:59 +0900

    chore: 진화, 깨달음, 도약 데이터 추가 #13

---
+++ src/data/arkpassive/common/leapData.ts
@@ -0,0 +1,52 @@
+export const leapData = [
+  {
+    name: '도약',
+    title: '초월적인 힘',
+    icon: 'https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_02/ark_passive_02_3.png',
+    level: 0,
+    maxLevel: 5,
+    isActive: true,
+  },
+  {
+    name: '도약',
+    title: '충전된 분노',
+    icon: 'https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_02/ark_passive_02_1.png',
+    level: 0,
+    maxLevel: 5,
+    isActive: false,
+  },
+  {
+    name: '도약',
+    title: '각성 증폭기',
+    icon: 'https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_01/ark_passive_01_54.png',
+    level: 0,
+    maxLevel: 3,
+    isActive: false,
+  },
+  {
+    name: '도약',
+    title: '풀려난 힘',
+    icon: 'https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_02/ark_passive_02_2.png',
+    level: 0,
+    maxLevel: 5,
+    isActive: true,
+  },
+  {
+    name: '도약',
+    title: '잠재력 해방',
+    icon: 'https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_01/ark_passive_01_10.png',
+    level: 0,
+    maxLevel: 5,
+    isActive: false,
+  },
+  {
+    name: '도약',
+    title: '즉각적인 주문',
+    icon: 'https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_02/ark_passive_02_5.png',
+    level: 0,
+    maxLevel: 3,
+    isActive: true,
+  },
+];
+
+export default leapData;
