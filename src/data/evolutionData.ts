f929da44d4a5e831c02b10b12c25e8e811b49f76
Arklimits <zaqokm2@gmail.com>
2025-04-06 12:13:29 +0900

    chore: 진화 데이터 추가 #13

---
+++ src/data/evolutionData.ts
@@ -0,0 +1,172 @@
+export const evolutionData = [
+  {
+    name: "치명",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_1.png",
+    level: 0,
+    maxLevel: 30,
+    isActive: false
+  },
+  {
+    name: "특화",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_2.png",
+    level: 0,
+    maxLevel: 30,
+    isActive: false
+  },
+  {
+    name: "신속",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_4.png",
+    level: 0,
+    maxLevel: 30,
+    isActive: false
+  },
+  {
+    name: "제압",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_3.png",
+    level: 0,
+    maxLevel: 30,
+    isActive: false
+  },
+  {
+    name: "인내",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_5.png",
+    level: 0,
+    maxLevel: 30,
+    isActive: false
+  },
+  {
+    name: "숙련",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_6.png",
+    level: 0,
+    maxLevel: 30,
+    isActive: false
+  },
+  {
+    name: "끝없는 마나",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_16.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "금단의 주문",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_12.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "예리한 감각",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_29.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "한계 돌파",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_34.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "최적화 훈련",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_22.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "축복의 여신",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_19.png",
+    level: 0,
+    maxLevel: 3,
+    isActive: false
+  },
+  {
+    name: "무한한 마력",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_14.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "혼신의 강타",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_27.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "일격",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_32.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "파괴 전차",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_35.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "타이밍 지배",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_23.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "정열의 춤사위",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_33.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "뭉툭한 가시",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_20.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "음속 돌파",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_21.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "인파이팅",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_38.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "입식 타격가",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_18.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "마나 용광로",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_24.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  },
+  {
+    name: "안정된 관리자",
+    icon: "https://cdn-lostark.game.onstove.com/efui_iconatlas/ark_passive_evolution/ark_passive_evolution_25.png",
+    level: 0,
+    maxLevel: 2,
+    isActive: false
+  }
+];
+
+export default evolutionData;
