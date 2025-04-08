a602e1ab5a519effa2c5716f011b6809a9fb49a3
Arklimits <zaqokm2@gmail.com>
2025-04-08 21:44:41 +0900

    feat: 새로운 스킬 받아오면 DB에 등록하도록 변경 #17

--- src/app/api/info/skill/route.ts
+++ src/app/api/info/skill/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { db } from '@/lib/db';
 import { RowDataPacket } from 'mysql2';
+import { extractCoolTimeFromTooltip } from '@/services/skill/extractUtils';
 
 export async function POST(req: NextRequest) {
-  const { skills } = await req.json();
+  const { skills, className } = await req.json();
 
   if (!Array.isArray(skills)) {
     return NextResponse.json({ error: '잘못된 요청입니다.' }, { status: 400 });
@@ -11,12 +12,36 @@ export async function POST(req: NextRequest) {
 
   const names = skills.map((s: any) => s.Name);
   const [constants] = await db.query<RowDataPacket[]>(
-    `SELECT name, coefficient, constant, cooltime, casttime FROM skills WHERE name IN (${names.map(() => '?').join(',')})`,
+    `SELECT name, icon, coefficient, constant, cooltime, casttime FROM skills WHERE name IN (${names.map(() => '?').join(',')})`,
+    names
+  );
+
+  const missingSkills = skills.filter(skill => !constants.some(c => c.name === skill.Name));
+
+  if (missingSkills.length > 0) {
+    const values = missingSkills.map(skill => [
+      skill.Name,
+      skill.Icon,
+      1, // coefficient
+      1, // constant
+      extractCoolTimeFromTooltip(skill.Tooltip), // cooltime
+      0, // casttime]
+      className,
+    ]);
+
+    await db.query(
+      `INSERT INTO skills (name, icon, coefficient, constant, cooltime, casttime, class) VALUES ?`,
+      [values]
+    );
+  }
+
+  const [updatedConstants] = await db.query<RowDataPacket[]>(
+    `SELECT name, icon, coefficient, constant, cooltime, casttime FROM skills WHERE name IN (${names.map(() => '?').join(',')})`,
     names
   );
 
   const enriched = skills.map(skill => {
-    const constant = constants.find(c => c.name === skill.Name);
+    const constant = updatedConstants.find(c => c.name === skill.Name);
 
     return {
       ...skill,
