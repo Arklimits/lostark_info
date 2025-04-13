ddf12ac4dcf8b0cbd49f9d37139bb3c6a825bbf3
arklimits <zaqokm2@gmail.com>
2025-04-14 06:55:53 +0900

    fix: 공지사항 클릭 시 전체 최대화 및 최소화 되는 문제 수정 #hotfix

--- src/containers/board/BoardContainer.tsx
+++ src/containers/board/BoardContainer.tsx
@@ -21,17 +21,32 @@ export const BoardContainer = () => {
     fetchBoardList();
   }, []);
 
-  const [isActive, setIsActive] = useState(false);
+  const [activeIndices, setActiveIndices] = useState<number[]>([]);
+
+  const toggleActive = (index: number) => {
+    setActiveIndices(prev => {
+      if (prev.includes(index)) {
+        return prev.filter(i => i !== index);
+      } else {
+        return [...prev, index];
+      }
+    });
+  };
+
   return (
     <div className={`${styles.container} ${styles.scrollBox}`}>
       <div className={styles.boardTitle}>공지사항</div>
       {boardList.map((board, index) => (
         <div className={styles.item} key={index}>
-          <h2 className={styles.title} onClick={() => setIsActive(!isActive)}>
+          <h2 className={styles.title} onClick={() => toggleActive(index)}>
             {board.title}
-            <span className={`${styles.accordionIcon} ${isActive ? styles.active : ''}`}></span>
+            <span
+              className={`${styles.accordionIcon} ${activeIndices.includes(index) ? styles.active : ''}`}
+            ></span>
           </h2>
-          <div className={`${styles.text} ${isActive ? styles.active : ''}`}>{board.text}</div>
+          <div className={`${styles.text} ${activeIndices.includes(index) ? styles.active : ''}`}>
+            {board.text}
+          </div>
         </div>
       ))}
     </div>
