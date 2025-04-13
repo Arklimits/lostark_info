2c1267158d7cef5770aaad69ebfcd0605c78e1e5
Arklimits <zaqokm2@gmail.com>
2025-04-14 02:53:33 +0900

    feat: 로고 클릭 시 메인페이지로 이동 #20

--- src/components/navbar/TopNavBar.tsx
+++ src/components/navbar/TopNavBar.tsx
@@ -1,11 +1,17 @@
+'use client';
+
 import styles from './TopNavBar.module.scss';
 import SearchBar from '../search/SearchBar';
+import { useRouter } from 'next/navigation';
 
 export const TopNavBar = () => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
       <div className={styles.navbar}>
-        <div className={styles.logo}>LOSTDAM</div>
+        <div className={styles.logo} onClick={() => router.push('/')}>
+          LOSTDAM
+        </div>
         <SearchBar className={styles.navSearchBar} />
       </div>
     </div>
